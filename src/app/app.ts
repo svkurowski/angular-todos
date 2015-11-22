6148f567ea17e905272a7d7a583186bbeed6b737
chocoloper <chocoloper@thneed.de>
2015-11-22 21:47:49 +0100

    added http

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import {bootstrap, provide, Component} from 'angular2/angular2';
 import {RouteConfig, Route, Redirect, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
+import {HTTP_PROVIDERS} from 'angular2/http';
 import {TodoList} from './services/todo-list';
 import {TodoListComponent} from './todo-list-component';
 import {TodoDetailComponent} from './todo-detail-component';
@@ -18,6 +19,7 @@ class AppComponent { }
 
 bootstrap(AppComponent, [
   TodoList,
+  HTTP_PROVIDERS,
   ROUTER_PROVIDERS,
   provide(LocationStrategy, {useClass: HashLocationStrategy})
 ]);
--- src/app/services/todo-list.ts
+++ src/app/services/todo-list.ts
@@ -1,11 +1,15 @@
 import {Injectable} from 'angular2/angular2';
+import {Http, Response} from 'angular2/http';
 import {TodoItem} from '../todo-item';
 
 @Injectable()
 export class TodoList {
-  todos: TodoItem[] = [
-    new TodoItem("Grocery shopping"),
-    new TodoItem("Make a fruit salad"),
-    new TodoItem("Get money")
-  ]
+  todos: TodoItem[] = [];
+
+  constructor(public http: Http) {
+    this.todos.push(new TodoItem("Write a todo list",true))
+    this.http.get('./app/todos.json')
+    .map((res:Response) => JSON.parse(res.text()))
+    .subscribe(res => this.todos = this.todos.concat(res));
+  }
 }
